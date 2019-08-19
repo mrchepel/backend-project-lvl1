0b6881b9f333ab81d34b803a00a9573b845a875c
mr_chepel <aleks.shev@gmail.com>
2019-08-19 14:03:27 +0000

    added game brain-calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,8 +2,8 @@ import { cons, car, cdr } from '@hexlet/pairs';
 import getRandomNumber from '../getRandomNumber';
 import game from '..';
 
-const getOperator = expression => car(expression);
-const getResult = expression => cdr(expression);
+const getOperator = (expression) => car(expression);
+const getResult = (expression) => cdr(expression);
 let countOperators = 0;
 const rules = 'What is the result of the expression?';
 
@@ -15,6 +15,8 @@ const getCalculation = (x, y, operator) => {
       return cons('-', x - y);
     case 2:
       return cons('*', x * y);
+    default:
+      return null;
   }
 };
 
@@ -27,6 +29,5 @@ const checkResultExpression = () => {
   countOperators += 1;
   return cons(question, answer);
 };
-
 
 export default () => game(rules, checkResultExpression);
